c3017d311f160d3c6cab471733e03f4966de7201
daniel <dcordonl@esliceu.net>
2026-01-16 15:48:13 +0100

    index

---
+++ php-recuperacio/index.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__ . "/db.php";
+$pageTitle = "Inicio";
+require __DIR__ . "/includes/header.php";
+?>
+
+<section class="hero">
+    <div class="hero-content">
+        <h1>Pagina de recuperació del examen</h1>
+        <button><a href="afegir_alumne.php">ENTRA</a></button>
+    </div>
+</section>
+
+<?php
+require __DIR__ . "/includes/footer.php";
+?>
